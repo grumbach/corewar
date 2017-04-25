e330da9f33faa75e1a81589fd5c41c1afd55d0ee
Antonin GAVREL <angavrel@e2r10p17.42.fr>
2017-04-25 04:54:19 +0200

    file for redcode

---
+++ srcs/corewar/scbw_redcode.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scbw_redcode.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <angavrel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/24 17:27:59 by angavrel          #+#    #+#             */
+/*   Updated: 2017/04/25 04:33:10 by angavrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** I will leave it to Anselm
+*/
+
+#include <corewar.h>
+
+void			rc_live(t_vm *vm, t_proc *proc)
+{
+	(void)vm;
+	(void)proc;
+	return ;
+}
+
+void			rc_ld(t_vm *vm, t_proc *proc, int redcode)
+{
+	(void)vm;
+	(void)proc;
+	return ;
+}
+
+void			rc_st(t_vm *vm, t_proc *proc)
+{
+	(void)vm;
+	(void)proc;
+	return ;
+}
+
+void			rc_add_sub(t_vm *vm, t_proc *proc, int plus_minus)
+{
+	(void)vm;
+	(void)proc;
+	return ;
+}
+
+void			rc_binary(t_vm *vm, t_proc *proc, int redcode)
+{
+	(void)vm;
+	(void)proc;
+	return ;
+}
+
+void			rc_zjmp(t_vm *vm, t_proc *proc)
+{
+	(void)vm;
+	(void)proc;
+	return ;
+}
+
+void			rc_ldi(t_vm *vm, t_proc *proc, int redcode)
+{
+	(void)vm;
+	(void)proc;
+	return ;
+}
+
+void			rc_sti(t_vm *vm, t_proc *proc)
+{
+	(void)vm;
+	(void)proc;
+	return ;
+}
+
+void			rc_fork(t_vm *vm, t_proc *proc, int redcode)
+{
+	(void)vm;
+	(void)proc;
+	return ;
+}
+
+void			rc_aff(t_vm *vm, t_proc *proc)
+{
+	(void)vm;
+	(void)proc;
+	return ;
+}
