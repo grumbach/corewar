dcd424fcc34937e1b14d3e65027e0f3e957c594f
Antonin GAVREL <angavrel@e2r11p12.42.fr>
2017-05-17 08:35:33 +0200

    some tests

---
+++ resources/tests/4add.s
@@ -0,0 +1,7 @@
+.name "see file name"
+.comment "..."
+
+ld %20, r2
+ld %22, r3
+add r2,r3,r1
+aff r1
---
+++ resources/tests/5sub.s
@@ -0,0 +1,7 @@
+.name "see file name"
+.comment "..."
+
+ld %65, r2
+ld %22, r3
+sub r2,r3,r1
+aff r1
